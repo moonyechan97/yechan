950c8c72cfa5b0a957d4a392ba25a2cf67ced8ca
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-03-07 16:52:47 +0900

    no message

---
+++ java-basic/src/kr/ac/kopo/day03/LoopMain01.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.day03;
+
+public class LoopMain01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("====== start ======");
+
+		int cnt = 1;
+		while(cnt <= 5) {
+			int a = 10;
+			System.out.println("Hello");
+			++cnt;
+		}
+		
+		//System.out.println("cnt : " + cnt);
+		System.out.println("====== end ======");
+		
+	}
+}
