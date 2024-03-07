e24fe092fbfd193a89c90dd7016ab5e9a4f5cc3f
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-03-07 10:58:35 +0900

    no message

---
+++ java-basic/src/kr/ac/kopo/day03/homework/ex01.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.day03.homework;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("---------------------------");
+		System.out.println("영문자의 아스키 코드 값");
+		System.out.println("---------------------------");
+		System.out.println("A = " + (int)'A');
+		System.out.println("E = " + (int)'E');
+		System.out.println("I = " + (int)'I');
+		System.out.println("O = " + (int)'O');
+		System.out.println("U = " + (int)'U');
+		System.out.println("---------------------------");
+	}
+
+}
+
