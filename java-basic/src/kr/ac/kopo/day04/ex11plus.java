1bebf3e5f57b5c696c16aa1558772525be13f9e4
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-03-11 20:27:37 +0900

    no message

---
+++ java-basic/src/kr/ac/kopo/day04/ex11plus.java
@@ -0,0 +1,22 @@
+package kr.ac.kopo.day04;
+
+public class ex11plus {
+
+	public static void main(String[] args) {
+		
+        int nStar = 1;
+		
+		for(int i = 1 ; i <= 9; i++) {
+			for(int j = 1; j <= nStar; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			if(i < 5) {
+				nStar++;
+			} else {
+				nStar--;
+			}
+		}
+	}
+}
+
