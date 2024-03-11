99e4b0056bcd8f15848ef944722f8a630bb26da9
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-03-12 08:58:43 +0900

    no message

---
+++ java-basic/src/kr/ac/kopo/day04/ex22.java
@@ -0,0 +1,21 @@
+package kr.ac.kopo.day04;
+
+import java.util.Scanner;
+
+public class ex22 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("시작단 : ");
+		int start = sc.nextInt();
+		
+		System.out.print("종료단 : ");
+		int end = sc.nextInt();
+		
+		
+
+	}
+
+}
