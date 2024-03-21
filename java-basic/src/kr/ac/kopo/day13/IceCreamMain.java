063b8fd5b0778c6f3f151b9568f7a5d1cb054298
DESKTOP-O3LMC10\User <skyfiy97@naver.com>
2024-03-21 19:26:33 +0900

    no message

---
+++ java-basic/src/kr/ac/kopo/day13/IceCreamMain.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.day13;
+
+public class IceCreamMain {
+
+	public static void main(String[] args) {
+		
+		IceCreamMarket market = new IceCreamMarket();
+		market.open();
+	}
+
+}
